0ab36f5bd28192ebab5947e53e603442d1176c00
BernardBackup <Yemkay300@gmail.com>
2024-03-06 16:17:25 +0000

    alt

---
+++ gia-improved/pages/account.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Welcome from '@/components/Project/index'
+
+const Account = () => {
+  return (
+    <div>
+        <Welcome/>
+    </div>
+  );
+};
+
+export default Account;
